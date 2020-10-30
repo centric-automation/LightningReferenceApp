370aa597314c243c27f06dc09e0ac7bf734a7bc1
Shawn Wallace <shawn@the-wallaces.net>
2020-10-30 15:00:20 -0400

    wokring on getting tests to twork

--- referenceApp/webapp/src/__tests__/helloWorld.tests.js
+++ referenceApp/webapp/src/__tests__/helloWorld.tests.js
@@ -1,9 +1,9 @@
 import { act } from "react-dom/test-utils";
 
-beforeEach(() => { });
-afterEach(() => { });
+beforeEach(() => { console.log("executing beforeEach"); });
+afterEach(() => { console.log("executing afterEach"); });
 
-it("hello world!", () =>{
-	act(() => {});
+it("hello, world!", () =>{
+	act(() => { console.log("acting"); });
 	expect(true).toBe(true);
 });
