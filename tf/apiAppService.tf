76c6cc77aaa6054efcd6fe1f60063190495984a9
Dusty VonHandorf <dusty.vonhandorf@centricconsulting.com>
2020-12-21 09:37:34 -0500

    Add application insights alerting

--- tf/apiAppService.tf
+++ tf/apiAppService.tf
@@ -16,6 +16,7 @@ resource "azurerm_app_service" "api" {
     "DOCKER_REGISTRY_SERVER_USERNAME"     = var.shared_container_registry_admin_username
     "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.shared_container_registry_admin_password
 		"DOCKER_CUSTOM_IMAGE_NAME" = "${var.shared_container_registry_login_server}/refrenceApp.api:latest"
+		"ApplicationInsights__ApplicationVersion" = "${var.shared_container_registry_login_server}/refrenceApp.api:latest"
 		"APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.insights.instrumentation_key
   }
 
@@ -33,4 +34,90 @@ resource "azurerm_app_service" "api" {
 output "api_url" {
   description = "API URL"
   value       = azurerm_app_service.api.default_site_hostname
+}
+
+resource "azurerm_application_insights_web_test" "api" {
+  name = "api"
+  location = azurerm_resource_group.app.location
+  resource_group_name = azurerm_resource_group.app.name
+  application_insights_id = azurerm_application_insights.insights.id
+  kind = "ping"
+  frequency = 300
+  timeout = 60
+  description = "Check to make sure API is available"
+  geo_locations = [
+    "us-va-ash-azr",
+    "us-ca-sjc-azr",
+    "us-fl-mia-edge",
+    "us-il-ch1-azr",
+    "us-tx-sn1-azr"
+  ]
+
+  configuration = <<XML
+<WebTest Name="api" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
+  <Items>
+    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="https://${azurerm_app_service.api.default_site_hostname}/api/Todo" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+}
+
+resource "azurerm_monitor_metric_alert" "api_failed_requests" {
+  name = "apiFailedRequests"
+  resource_group_name = azurerm_resource_group.app.name
+
+  scopes = [azurerm_app_service.api.id]
+  description = "Triggered when failed requests rise above background noise"
+
+  criteria {
+    metric_namespace = "microsoft.web/sites"
+    metric_name = "Http5xx"
+    aggregation = "Count"
+    operator = "GreaterThanOrEqual"
+    threshold = "3"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.performance_alert.id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "api_response_time" {
+  name = "apiResponseTime"
+  resource_group_name = azurerm_resource_group.app.name
+
+  scopes = [azurerm_app_service.api.id]
+  description = "Triggered when response time rises above SLA"
+
+  criteria {
+    metric_namespace = "microsoft.web/sites"
+    metric_name = "HttpResponseTime"
+    aggregation = "Average"
+    operator = "GreaterThanOrEqual"
+    threshold = "0.5"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.performance_alert.id
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "api_pending_requests" {
+  name = "apiPendingRequests"
+  resource_group_name = azurerm_resource_group.app.name
+
+  scopes = [azurerm_app_service.api.id]
+  description = "Triggered when pending request count rises above SLA"
+
+  criteria {
+    metric_namespace = "microsoft.web/sites"
+    metric_name = "RequestsInApplicationQueue"
+    aggregation = "Average"
+    operator = "GreaterThanOrEqual"
+    threshold = "10"
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.performance_alert.id
+  }
 }
--- tf/appInsights.tf
+++ tf/appInsights.tf
@@ -12,3 +12,9 @@ output "instrumentation_key" {
 output "app_id" {
   value = azurerm_application_insights.insights.app_id
 }
+
+resource "azurerm_monitor_action_group" "performance_alert" {
+  name = "PerformanceAlert"
+  resource_group_name = azurerm_resource_group.app.name
+  short_name = "perfAlert"
+}
