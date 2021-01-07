b55d61d87675d114f214add6b4cc8fd80b12f48d
Dusty VonHandorf <dusty.vonhandorf@centricconsulting.com>
2021-01-07 15:12:13 -0500

    Remove stale web test

--- tf/apiAppService.tf
+++ tf/apiAppService.tf
@@ -36,32 +36,6 @@ output "api_url" {
   value       = azurerm_app_service.api.default_site_hostname
 }
 
-resource "azurerm_application_insights_web_test" "api" {
-  name = "api"
-  location = azurerm_resource_group.app.location
-  resource_group_name = azurerm_resource_group.app.name
-  application_insights_id = azurerm_application_insights.insights.id
-  kind = "ping"
-  frequency = 300
-  timeout = 60
-  description = "Check to make sure API is available"
-  geo_locations = [
-    "us-va-ash-azr",
-    "us-ca-sjc-azr",
-    "us-fl-mia-edge",
-    "us-il-ch1-azr",
-    "us-tx-sn1-azr"
-  ]
-
-  configuration = <<XML
-<WebTest Name="api" Id="ABD48585-0831-40CB-9069-682EA6BB3583" Enabled="True" CssProjectStructure="" CssIteration="" Timeout="0" WorkItemIds="" xmlns="http://microsoft.com/schemas/VisualStudio/TeamTest/2010" Description="" CredentialUserName="" CredentialPassword="" PreAuthenticate="True" Proxy="default" StopOnError="False" RecordedResultFile="" ResultsLocale="">
-  <Items>
-    <Request Method="GET" Guid="a5f10126-e4cd-570d-961c-cea43999a200" Version="1.1" Url="https://${azurerm_app_service.api.default_site_hostname}/api/Todo" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
-  </Items>
-</WebTest>
-XML
-}
-
 resource "azurerm_monitor_metric_alert" "api_failed_requests" {
   name = "apiFailedRequests"
   resource_group_name = azurerm_resource_group.app.name
